35d452f011f4742e42be67e05417e964ac317a44
AminHarroudj <amin.harroudj.2002@gmail.com>
2024-11-08 12:10:05 +0100

    update

--- client/src/pages/ContactUs.jsx
+++ client/src/pages/ContactUs.jsx
@@ -4,7 +4,7 @@ import Location from "../components/Location";
 function ContactUs() {
   return (
     <>
-      <div className="flex flex-col items-center pt-24 md:pt-44">
+      <div className="flex flex-col items-center pt-32 md:pt-44">
         <div className="flex flex-col justify-center text-center items-center gap-4">
           <h1 className="text-7xl md:text-9xl font-semibold">Contact Us</h1>
           <p className="text-lg md:text-2xl opacity-50">
